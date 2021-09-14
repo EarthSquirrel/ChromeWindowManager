4a19364f81d73c0721915fda4dae4804d1d682ad
EarthSquirrel <britneyandcaddy1@gmail.com>
2021-09-14 12:57:56 -0600

    Print lots of tab info to console

--- window.js
+++ window.js
@@ -1,4 +1,9 @@
 // https://www.codeproject.com/Questions/5253015/How-to-get-all-tabs-details-from-current-browser-i
+
+function handleWindows(windows) {
+  
+}
+
 window.onload = function() {
   let btnName = document.getElementById("btnName");
   console.log('btn clicked');
@@ -7,22 +12,27 @@ window.onload = function() {
   console.log(window.name);
   windowNameP.innerText = "window name: " + window.name;
   nameDiv.appendChild(windowNameP);
-  var tabs = [];
-  var allWin = chrome.windows.getAll({"populate": true}, function (windows) {
-    console.log(windows.length);
+  //console.log('len win tabs: ' + windowTabs.length);
+  //var winTabs = getWindowTabs();
+  var windowTabs = [];
+  chrome.windows.getAll({"populate": true}, function (windows) {
+    console.log('windows.length: ' + windows.length);
     for (var i=0; i < windows.length; i++) {
       let win = windows[i];
+      console.log('window name: ' + win.name);
+      var tabs = [];
       console.log(i + ' : ' + win.tabs.length);
       for (var j=0; j<win.tabs.length; j++) {
         var t = win.tabs[j];
-        //if (isValidTab(t)) {
-          tabs.push(t);
-        //}
+        tabs.push(t);
+        console.log(t.url);
       }
+      windowTabs.push(tabs);
+      console.log('for loog len: ' + windowTabs.length);
     }
-    //sendResponse({ tabs: tabs });
+    console.log('after for len: ' + windowTabs.length);
+    return windowTabs;
   });
-  for (let tab of tabs) {
-    console.log(tab);
-  }
+  console.log('meth length: ' + windowTabs.length);
+  console.log('winTabs length: ' + windowTabs.length);
 }
