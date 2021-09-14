f1d04abee67bfb17c8a4d8d2378957838ad0ed8e
EarthSquirrel <britneyandcaddy1@gmail.com>
2021-09-14 13:32:46 -0600

    List tabs under window and remove irrelevant code

--- window.js
+++ window.js
@@ -1,39 +1,54 @@
 // https://www.codeproject.com/Questions/5253015/How-to-get-all-tabs-details-from-current-browser-i
 
 function processWindow(win, i) {
-  var name = win.name;
-  var tabs = [];
+  let name = win.name;
+  let tabs = [];
   console.log('processing : ' + win.tabs.length);
-  for (var j=0; j<win.tabs.length; j++) {
-    var t = win.tabs[j];
+  for (let j=0; j<win.tabs.length; j++) {
+    let t = win.tabs[j];
     tabs.push(t);
   }
 
   // create document option
-  var div = document.createElement("DIV");
+  let div = document.createElement("DIV");
   div.id = "window " + i;
-  var h1 = document.createElement('H1');
-  h1.innerText = 'Window: ' + win.name;
-  div.appendChild(h1);
+  let h2 = document.createElement('H2');
+  h2.innerText = 'Window: ' + win.name;
+  div.appendChild(h2);
+  
   //var renameForm = document.createElement("FORM");
+  let tabsH3 = document.createElement('H3');
+  tabsH3.innerText = "Tabs";
+  div.appendChild(tabsH3);
+  
+  //make list of all tabs
+  let ul = document.createElement('UL');
+  for (let t=0; t<tabs.length; t++) {
+    let tab = tabs[t];
+    let li = document.createElement('LI');
+    let a = document.createElement('A');
+    let linkText = document.createTextNode(tab.title);
+    a.appendChild(linkText);
+    a.title = tab.title;
+    a.href = tab.url;
+    li.appendChild(a);
+    ul.appendChild(li);
+  }
+  div.appendChild(ul);
+
 
   return div;
 }
 
+var wins = [];
 window.onload = function() {
-  let btnName = document.getElementById("btnName");
-  console.log('btn clicked');
-  let nameDiv = document.getElementById("showWindows");
-  let windowNameP = document.createElement("P");
-  console.log(window.name);
-  windowNameP.innerText = "window name: " + window.name;
-  nameDiv.appendChild(windowNameP);
   
   chrome.windows.getAll({"populate": true}, function (windows) {
-    var windowDiv = document.getElementById("windowInfo");
+    let windowDiv = document.getElementById("windowInfo");
     console.log('windows.length: ' + windows.length);
-    for (var i=0; i < windows.length; i++) {
+    for (let i=0; i < windows.length; i++) {
       let win = windows[i];
+      wins.push(win);
       windowDiv.appendChild(processWindow(win, i));
     }
   });
