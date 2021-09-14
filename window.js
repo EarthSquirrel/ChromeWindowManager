f70ce317490b4787c31ecd5ea4e8d1f2d7998231
EarthSquirrel <britneyandcaddy1@gmail.com>
2021-09-14 13:14:16 -0600

    Added div for each window

--- window.js
+++ window.js
@@ -1,7 +1,23 @@
 // https://www.codeproject.com/Questions/5253015/How-to-get-all-tabs-details-from-current-browser-i
 
-function handleWindows(windows) {
-  
+function processWindow(win, i) {
+  var name = win.name;
+  var tabs = [];
+  console.log('processing : ' + win.tabs.length);
+  for (var j=0; j<win.tabs.length; j++) {
+    var t = win.tabs[j];
+    tabs.push(t);
+  }
+
+  // create document option
+  var div = document.createElement("DIV");
+  div.id = "window " + i;
+  var h1 = document.createElement('H1');
+  h1.innerText = 'Window: ' + win.name;
+  div.appendChild(h1);
+  //var renameForm = document.createElement("FORM");
+
+  return div;
 }
 
 window.onload = function() {
@@ -12,27 +28,13 @@ window.onload = function() {
   console.log(window.name);
   windowNameP.innerText = "window name: " + window.name;
   nameDiv.appendChild(windowNameP);
-  //console.log('len win tabs: ' + windowTabs.length);
-  //var winTabs = getWindowTabs();
-  var windowTabs = [];
+  
   chrome.windows.getAll({"populate": true}, function (windows) {
+    var windowDiv = document.getElementById("windowInfo");
     console.log('windows.length: ' + windows.length);
     for (var i=0; i < windows.length; i++) {
       let win = windows[i];
-      console.log('window name: ' + win.name);
-      var tabs = [];
-      console.log(i + ' : ' + win.tabs.length);
-      for (var j=0; j<win.tabs.length; j++) {
-        var t = win.tabs[j];
-        tabs.push(t);
-        console.log(t.url);
-      }
-      windowTabs.push(tabs);
-      console.log('for loog len: ' + windowTabs.length);
+      windowDiv.appendChild(processWindow(win, i));
     }
-    console.log('after for len: ' + windowTabs.length);
-    return windowTabs;
   });
-  console.log('meth length: ' + windowTabs.length);
-  console.log('winTabs length: ' + windowTabs.length);
 }
