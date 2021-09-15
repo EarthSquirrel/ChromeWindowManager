0318e2bc3e8e466e38c1f0bbb7ffcd632f99d8d0
EarthSquirrel <britneyandcaddy1@gmail.com>
2021-09-14 20:26:19 -0600

    Added button to clear local storage

--- popup.js
+++ popup.js
@@ -3,3 +3,8 @@ let button = document.getElementById("openWindow");
 button.onclick = function() {
   chrome.tabs.create({url: 'window.html'});
 }
+
+let btnClear = document.getElementById("clearStorage");
+btnClear.onclick = function() {
+  chrome.storage.local.clear();
+}
