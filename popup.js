03b5dddf4dc70d84657a27c81e2c349f24a2c7ac
EarthSquirrel <britneyandcaddy1@gmail.com>
2021-09-15 05:08:44 -0600

    Added closed windows to popup menu

--- popup.js
+++ popup.js
@@ -8,3 +8,35 @@ let btnClear = document.getElementById("clearStorage");
 btnClear.onclick = function() {
   chrome.storage.local.clear();
 }
+
+window.onload = function() {
+  let closedWindows = document.getElementById('showClosedWindows');
+  let maxWidth = 0;
+  chrome.storage.local.get(null, function(items) {
+    let allKeys = Object.keys(items);
+    for (let i=0; i<allKeys.length; i++) {
+      console.log(allKeys[i]);
+      chrome.storage.local.get(allKeys[i], function (obj) {
+        let key = allKeys[i];
+        let win = obj[key];
+        if (win.type == "closedWindow") {
+          let rowDiv = document.createElement('div');
+          rowDiv.className = "showClosedWindows";
+          let btn = document.createElement('button');
+          btn.innerText = "open";
+          let lab = document.createElement('label');
+          lab.className = "showClosedWindows";
+          lab.innerText = win.name;
+          rowDiv.appendChild(btn);
+          rowDiv.appendChild(lab);
+          let compWidth = lab.offsetWidth + btn.offsetWidth;
+          if (maxWidth < compWidth) {
+            maxWidth = compWidth;
+          }
+          closedWindows.appendChild(rowDiv);
+        }
+      });
+    }
+  });
+
+}
